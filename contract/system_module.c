ff278696b51fc19f945df54d7a7b148737846ba6
Kyungtae Lee <mlogue76@gmail.com>
2018-12-05 11:25:20 +0900

    [chore] Fix a problem where build fails on Windows

--- contract/system_module.c
+++ contract/system_module.c
@@ -286,7 +286,11 @@ static int os_time(lua_State *L)
         ts.tm_mon = getfield(L, "month", -1) - 1;
         ts.tm_year = getfield(L, "year", -1) - 1900;
         ts.tm_isdst = getboolfield(L, "isdst");
+#if LJ_TARGET_POSIX
         t = timegm(&ts);
+#else
+        t = _mkgmtime(&ts);
+#endif
     }
     if (t == (time_t)(-1))
         lua_pushnil(L);
