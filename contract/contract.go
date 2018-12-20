63459b8eac3d773ecd0fe79f94cb0efaf2bfa8a7
Kyungtae Lee <mlogue76@gmail.com>
2018-12-20 16:17:16 +0900

    [ctr/lua] Suppress insufficient balance errors

--- contract/contract.go
+++ contract/contract.go
@@ -100,7 +100,7 @@ func Execute(bs *state.BlockState, tx *types.Tx, blockNo uint64, ts int64, prevB
 		}
 	}
 	if err != nil {
-		if err == types.ErrInsufficientBalance || err == types.ErrVmStart {
+		if err == types.ErrVmStart {
 			return "", err
 		} else if _, ok := err.(DbSystemError); ok {
 			return "", err
