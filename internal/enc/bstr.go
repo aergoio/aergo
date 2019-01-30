f2394381d3b5cbdc412ae2963adf7296e745e530
Hayarobi Park <hayarobipark@gmail.com>
2019-01-30 12:14:16 +0900

    [MISC] Clarify documentation

--- internal/enc/bstr.go
+++ internal/enc/bstr.go
@@ -2,12 +2,12 @@ package enc
 
 import "github.com/mr-tron/base58/base58"
 
-// ToString returns human-readable (base64) string from b
+// ToString returns human-readable (base58) string from b. Nil parameter return empty string, but not inversable; i.e. calling ToBytes() with empty string parameter will return zero length string error.
 func ToString(b []byte) string {
 	return base58.Encode(b)
 }
 
-// ToBytes returns byte slice from human-readable (base64) string
+// ToBytes returns byte slice from human-readable (base58) string
 func ToBytes(s string) ([]byte, error) {
 	return base58.Decode(s)
 }
