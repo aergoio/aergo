6cf337489e6b40a2f41e649339d0efadbae3eeb1
eve2adam <eve2adam@gmail.com>
2019-02-28 13:31:09 +0900

    [ctr/lua] add error checking at event

--- contract/contract_module.c
+++ contract/contract_module.c
@@ -210,7 +210,6 @@ static int moduleSend(lua_State *L)
 static int moduleBalance(lua_State *L)
 {
 	char *contract;
-	int ret;
 	int *service = (int *)getLuaExecContext(L);
 	lua_Integer amount;
 
@@ -223,7 +222,7 @@ static int moduleBalance(lua_State *L)
     else {
 	    contract = (char *)luaL_checkstring(L, 1);
 	}
-	if ((ret = LuaGetBalance(L, service, contract)) < 0) {
+	if (LuaGetBalance(L, service, contract) < 0) {
 		lua_error(L);
 	}
 
@@ -315,7 +314,9 @@ static int moduleEvent(lua_State *L)
 	if (json_args == NULL) {
 		lua_error(L);
 	}
-	LuaEvent(L, service, event_name, json_args);
+	if (LuaEvent(L, service, event_name, json_args) < 0) {
+	    lua_error(L);
+	}
 	free(json_args);
 	return 0;
 }
