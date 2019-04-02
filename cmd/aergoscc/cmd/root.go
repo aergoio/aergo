a87dd4a8a14737efc030beb3b09d56c57ffe5cca
wrpark <wrpark@blocko.io>
2019-04-02 16:49:29 +0900

    [build] Apply merged WAVM

--- cmd/aergoscc/cmd/root.go
+++ cmd/aergoscc/cmd/root.go
@@ -9,8 +9,6 @@ package cmd
 #cgo CFLAGS: -I${SRCDIR}/../../../contract/native -I${SRCDIR}/../../../libtool/include
 #cgo LDFLAGS: ${SRCDIR}/../../../contract/native/lib/libascl.a
 #cgo LDFLAGS: -L${SRCDIR}/../../../libtool/lib -lgmp -lbinaryen -lstdc++ -lm
-#cgo LDFLAGS: -L${SRCDIR}/../../../libtool/lib -lASCLVM -lRuntime -lWASTParse -lWASM -lRegExp -lNFA -lLLVMJIT -lIR -lLogging -lPlatform -lWAVMUnwind
-#cgo LDFLAGS: -lLLVMSupport -lLLVMCore -lLLVMPasses -lLLVMOrcJIT -lLLVMRuntimeDyld -lLLVMDebugInfoDWARF -lLLVMX86CodeGen -lLLVMX86AsmParser -lLLVMX86AsmPrinter -lLLVMX86Desc -lLLVMX86Disassembler -lLLVMX86Info -lLLVMX86Utils -lLLVMipo -lLLVMInstrumentation -lLLVMVectorize -lLLVMIRReader -lLLVMAsmParser -lLLVMLinker -lLLVMExecutionEngine -lLLVMRuntimeDyld -lLLVMAsmPrinter -lLLVMDebugInfoCodeView -lLLVMDebugInfoMSF -lLLVMGlobalISel -lLLVMSelectionDAG -lLLVMCodeGen -lLLVMScalarOpts -lLLVMInstCombine -lLLVMBitWriter -lLLVMTarget -lLLVMTransformUtils -lLLVMAnalysis -lLLVMProfileData -lLLVMX86AsmPrinter -lLLVMObject -lLLVMMCParser -lLLVMBitReader -lLLVMMCDisassembler -lLLVMMC -lLLVMX86Utils -lLLVMCore -lLLVMBinaryFormat -lLLVMSupport -lz -ltinfo -lpthread -lm -lLLVMDemangle -lrt -ldl
 
 #include "compile.h"
 */
@@ -35,17 +33,12 @@ var (
 	stackSize uint32
 
 	rootCmd = &cobra.Command{
-		Use:   "aergoscc [flags] file",
-		Short: "Aergo smart contract compiler",
+		Use: "aergoscc [flags] file",
 		Run: func(cmd *cobra.Command, args []string) {
 			if version {
 				fmt.Println(fmt.Sprintf("Aergo smart contract compiler %s", githash))
 			} else if len(args) < 1 {
 				fmt.Println("Error: no input files")
-				cmd.Usage()
-				os.Exit(1)
-			} else if stackSize%65536 != 0 {
-				fmt.Println("Error: stack size should be a multiple of 64KB")
 				os.Exit(1)
 			} else {
 				flags := C.flag_t{}
@@ -86,7 +79,7 @@ func init() {
 	rootCmd.Flags().BoolVar(&printLex, "print-lex", false, "")
 	rootCmd.Flags().BoolVar(&printYacc, "print-yacc", false, "")
 	rootCmd.Flags().BoolVar(&printWat, "print-wat", false, "")
-	rootCmd.Flags().Uint32Var(&stackSize, "stack-size", 0x100000, "")
+	rootCmd.Flags().Uint32Var(&stackSize, "stack", 0x100000, "")
 	rootCmd.Flags().BoolVarP(&version, "version", "v", false, "")
 
 	rootCmd.SetUsageFunc(func(cmd *cobra.Command) error {
@@ -94,15 +87,15 @@ func init() {
 		fmt.Println("  aergoscc [options] file...")
 		fmt.Println("")
 		fmt.Println("Options:")
-		fmt.Println("  --help                   Display this information")
-		fmt.Println("  --version                Display the version of the compiler")
+		fmt.Println("  -h,--help                Display this information")
+		fmt.Println("  -v,--version             Display the version of the compiler")
 		fmt.Println("")
 		fmt.Println("  -g                       Generate debug information")
-		fmt.Println("  -O<level>                Set the optimization level (default 2)")
-		fmt.Println("                           <level> should be 0, 1 or 2")
+		fmt.Println("  -O0,-O1,-O2              Set the optimization level (default 2)")
+		fmt.Println("")
 		fmt.Println("  --print-wat              Print WebAssembly text format to stdout")
-		fmt.Println("  --stack-size <size>      Set the maximum stack size (default 1048576)")
-		fmt.Println("                           <size> should be a multiple of 64KB")
+		fmt.Println("  --stack <size>           Set the maximum stack size (default 1048576)")
+		fmt.Println("")
 		return nil
 	},
 	)
