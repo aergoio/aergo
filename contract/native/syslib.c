9652eebd0707a42d38bf0740a79f115a87fd6299
wrpark <wrpark@blocko.io>
2019-04-26 10:02:06 +0900

    [ascl] Implement lower(), upper() functions

--- contract/native/syslib.c
+++ contract/native/syslib.c
@@ -18,21 +18,20 @@
 
 char *lib_src =
 "library system {\n"
-"    func abs32(int32 i) int32 : \"__abs32\";\n"
-"    func abs64(int64 i) int64 : \"__abs64\";\n"
-"    func abs128(int128 i) int128 : \"__mpz_abs\";\n"
+"    func abs32(int32 v) int32 : \"__abs32\";\n"
+"    func abs64(int64 v) int64 : \"__abs64\";\n"
+"    func abs128(int128 v) int128 : \"__mpz_abs\";\n"
 
 "    func pow32(int32 x, int32 y) int32 : \"__pow32\";\n"
 "    func pow64(int64 x, int32 y) int64 : \"__pow64\";\n"
-"    func pow128(int128 x, int32 y) int128 : \"__mpz_pow_ui\";\n"
+"    func pow128(int128 x, int32 y) int128 : \"__mpz_pow\";\n"
 
-"    func sign32(int32 x) int8 : \"__sign32\";\n"
-"    func sign64(int64 x) int8 : \"__sign64\";\n"
-"    func sign128(int128 x) int8 : \"__mpz_sign\";\n"
+"    func sign32(int32 v) int8 : \"__sign32\";\n"
+"    func sign64(int64 v) int8 : \"__sign64\";\n"
+"    func sign128(int128 v) int8 : \"__mpz_sign\";\n"
 
-"    func sqrt32(int32 x) int32 : \"__sqrt32\";\n"
-"    func sqrt64(int64 x) int64 : \"__sqrt64\";\n"
-"    func sqrt128(int128 x) int128 : \"__mpz_sqrt\";\n"
+"    func lower(string v) string : \"__lower\";\n"
+"    func upper(string v) string : \"__upper\";\n"
 "}";
 
 sys_fn_t sys_fntab_[FN_MAX] = {
