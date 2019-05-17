7e4bcba9d38c16f192c111e7a45bf08be82b75aa
eve2adam <eve2adam@gmail.com>
2019-05-17 13:51:41 +0900

    Revert "[ctr/lua] bug fix for json format of event argument"
    This reverts commit 927a8fee

--- contract/contract_module.c
+++ contract/contract_module.c
@@ -355,7 +355,7 @@ static int moduleEvent(lua_State *L)
 	}
 
 	event_name = (char *)luaL_checkstring(L, 1);
-	json_args = lua_util_get_json_from_stack (L, 2, lua_gettop(L), true);
+	json_args = lua_util_get_json_from_stack (L, 2, lua_gettop(L), false);
 	if (json_args == NULL) {
 		luaL_throwerror(L);
 	}
