ceaf04af26ce81c0527c32c3c0e1f8d85c8a442d
Hayarobi Park <hayarobipark@gmail.com>
2019-06-19 18:59:31 +0900

    [P2P] Fix occational test failure.

--- p2p/blkreceiver_test.go
+++ p2p/blkreceiver_test.go
@@ -78,6 +78,89 @@ func TestBlocksChunkReceiver_ReceiveResp(t *testing.T) {
 	tests := []struct {
 		name        string
 		input       []message.BlockHash
+		blkInput    [][]*types.Block
+
+		// to verify
+		consumed  int
+		respError bool
+	}{
+		{"TSingleResp", inputHashes,  [][]*types.Block{inputBlocks}, 1,  false},
+		{"TMultiResp", inputHashes, [][]*types.Block{inputBlocks[:1], inputBlocks[1:3], inputBlocks[3:]}, 1,  false},
+		// Fail1 remote err
+		{"TRemoteFail", inputHashes, [][]*types.Block{inputBlocks[:0]}, 1,  true},
+		// server didn't sent last parts. and it is very similar to timeout
+		//{"TNotComplete", inputHashes, time.Minute,0,[][]*types.Block{inputBlocks[:2]},1,0, false},
+		// Fail2 missing some blocks in the middle
+		{"TMissingBlk", inputHashes, [][]*types.Block{inputBlocks[:1],inputBlocks[2:3],inputBlocks[3:]},0, true},
+		// Fail2-1 missing some blocks in last
+		{"TMissingBlkLast", inputHashes, [][]*types.Block{inputBlocks[:1],inputBlocks[1:2],inputBlocks[3:]},1, true},
+		// Fail3 unexpected block
+		{"TDupBlock", inputHashes, [][]*types.Block{inputBlocks[:2],inputBlocks[1:3],inputBlocks[3:]},0, true},
+		{"TTooManyBlks", inputHashes[:4], [][]*types.Block{inputBlocks[:1],inputBlocks[1:3],inputBlocks[3:]},1, true},
+		{"TTooManyBlksMiddle", inputHashes[:2], [][]*types.Block{inputBlocks[:1],inputBlocks[1:3],inputBlocks[3:]},0, true},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			//mockContext := new(mockContext)
+			mockActor := p2pmock.NewMockActorService(ctrl)
+			mockActor.EXPECT().TellRequest(message.SyncerSvc, gomock.Any()).
+				DoAndReturn(func(a string, arg *message.GetBlockChunksRsp) {
+					if !((arg.Err != nil) == test.respError) {
+						t.Fatalf("Wrong error (have %v)\n", arg.Err)
+					}
+					if arg.Seq != seqNo {
+						t.Fatalf("Wrong seqNo %d, want %d)\n", arg.Seq, seqNo)
+					}
+				}).Times(1)
+
+			mockMF := p2pmock.NewMockMoFactory(ctrl)
+			mockMo := createDummyMo(ctrl)
+			mockMF.EXPECT().NewMsgBlockRequestOrder(gomock.Any(), gomock.Any(), gomock.Any()).Return(mockMo)
+			mockPeer := p2pmock.NewMockRemotePeer(ctrl)
+			mockPeer.EXPECT().ID().Return(dummyPeerID).AnyTimes()
+			mockPeer.EXPECT().MF().Return(mockMF)
+			mockPeer.EXPECT().SendMessage(gomock.Any()).Times(1)
+			if test.consumed > 0 {
+				mockPeer.EXPECT().ConsumeRequest(gomock.Any()).Times(test.consumed)
+			}
+
+			//expire := time.Now().Add(test.ttl)
+			br := NewBlockReceiver(mockActor, mockPeer, seqNo, test.input, time.Minute<<2)
+			br.StartGet()
+
+			msg := p2pcommon.NewSimpleMsgVal(subproto.GetBlocksResponse, sampleMsgID)
+			for i, blks := range test.blkInput {
+				body := &types.GetBlockResponse{Blocks: blks, HasNext: i < len(test.blkInput)-1}
+				br.ReceiveResp(msg, body)
+				if br.status == receiverStatusFinished {
+					break
+				}
+			}
+
+		})
+	}
+}
+
+
+func TestBlocksChunkReceiver_ReceiveRespTimeout(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	chain.Init(1<<20 , "", false, 1, 1 )
+
+	seqNo := uint64(8723)
+	blkNo := uint64(100)
+	prevHash := dummyBlockHash
+	inputHashes := make([]message.BlockHash, len(sampleBlks))
+	inputBlocks := make([]*types.Block, len(sampleBlks))
+	for i, hash := range sampleBlks {
+		inputHashes[i] = hash
+		inputBlocks[i] = &types.Block{Hash: hash, Header: &types.BlockHeader{PrevBlockHash: prevHash, BlockNo: blkNo}}
+		blkNo++
+		prevHash = hash
+	}
+	tests := []struct {
+		name        string
+		input       []message.BlockHash
 		ttl         time.Duration
 		blkInterval time.Duration
 		blkInput    [][]*types.Block
@@ -85,24 +168,9 @@ func TestBlocksChunkReceiver_ReceiveResp(t *testing.T) {
 		// to verify
 		consumed  int
 		sentResp  int
-		respError bool
 	}{
-		{"TSingleResp", inputHashes, time.Minute, 0, [][]*types.Block{inputBlocks}, 1, 1, false},
-		{"TMultiResp", inputHashes, time.Minute, 0, [][]*types.Block{inputBlocks[:1], inputBlocks[1:3], inputBlocks[3:]}, 1, 1, false},
-		// Fail1 remote err
-		{"TRemoteFail", inputHashes, time.Minute, 0, [][]*types.Block{inputBlocks[:0]}, 1, 1, true},
-		// server didn't sent last parts. and it is very similar to timeout
-		//{"TNotComplete", inputHashes, time.Minute,0,[][]*types.Block{inputBlocks[:2]},1,0, false},
-		// Fail2 missing some blocks in the middle
-		{"TMissingBlk", inputHashes, time.Minute,0,[][]*types.Block{inputBlocks[:1],inputBlocks[2:3],inputBlocks[3:]},0,1, true},
-		// Fail2-1 missing some blocks in last
-		{"TMissingBlkLast", inputHashes, time.Minute,0,[][]*types.Block{inputBlocks[:1],inputBlocks[1:2],inputBlocks[3:]},1,1, true},
-		// Fail3 unexpected block
-		{"TDupBlock", inputHashes, time.Minute,0,[][]*types.Block{inputBlocks[:2],inputBlocks[1:3],inputBlocks[3:]},0,1, true},
-		{"TTooManyBlks", inputHashes[:4], time.Minute*4,0,[][]*types.Block{inputBlocks[:1],inputBlocks[1:3],inputBlocks[3:]},1,1, true},
-		{"TTooManyBlksMiddle", inputHashes[:2], time.Minute,0,[][]*types.Block{inputBlocks[:1],inputBlocks[1:3],inputBlocks[3:]},0,1, true},
 		// Fail4 response sent after timeout
-		{"TTimeout", inputHashes, time.Millisecond * 200, time.Millisecond * 400, [][]*types.Block{inputBlocks[:1], inputBlocks[1:3], inputBlocks[3:]}, 1, 0, false},
+		{"TBefore", inputHashes, time.Millisecond * 40, time.Millisecond * 100, [][]*types.Block{inputBlocks[:1], inputBlocks[1:3], inputBlocks[3:]}, 1, 0},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
@@ -111,7 +179,8 @@ func TestBlocksChunkReceiver_ReceiveResp(t *testing.T) {
 			if test.sentResp > 0 {
 				mockActor.EXPECT().TellRequest(message.SyncerSvc, gomock.Any()).
 					DoAndReturn(func(a string, arg *message.GetBlockChunksRsp) {
-						if !((arg.Err != nil) == test.respError) {
+						// timeout should resp with timeout error or not send response
+						if arg.Err == nil {
 							t.Fatalf("Wrong error (have %v)\n", arg.Err)
 						}
 						if arg.Seq != seqNo {
@@ -127,7 +196,9 @@ func TestBlocksChunkReceiver_ReceiveResp(t *testing.T) {
 			mockPeer.EXPECT().ID().Return(dummyPeerID).AnyTimes()
 			mockPeer.EXPECT().MF().Return(mockMF)
 			mockPeer.EXPECT().SendMessage(gomock.Any()).Times(1)
-			mockPeer.EXPECT().ConsumeRequest(gomock.Any()).Times(test.consumed) //mock.AnythingOfType("p2pcommon.MsgID"))
+			if test.consumed > 0 {
+				mockPeer.EXPECT().ConsumeRequest(gomock.Any()).MinTimes(test.consumed)
+			}
 
 			//expire := time.Now().Add(test.ttl)
 			br := NewBlockReceiver(mockActor, mockPeer, seqNo, test.input, test.ttl)
@@ -135,9 +206,8 @@ func TestBlocksChunkReceiver_ReceiveResp(t *testing.T) {
 
 			msg := p2pcommon.NewSimpleMsgVal(subproto.GetBlocksResponse, sampleMsgID)
 			for i, blks := range test.blkInput {
-				if test.blkInterval > 0 {
-					time.Sleep(test.blkInterval)
-				}
+				time.Sleep(test.blkInterval)
+
 				body := &types.GetBlockResponse{Blocks: blks, HasNext: i < len(test.blkInput)-1}
 				br.ReceiveResp(msg, body)
 				if br.status == receiverStatusFinished {
