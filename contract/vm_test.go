e4bd142cd02f691d4d4daf3bf488fbc82de7155f
Kyungtae Lee <mlogue76@gmail.com>
2019-07-15 20:42:03 +0900

    [ctr/lua] Remove a global variable

--- contract/vm_test.go
+++ contract/vm_test.go
@@ -4839,4 +4839,94 @@ abi.register(abc, query)
 	}
 }
 
+func TestNDeploy(t *testing.T) {
+	bc, err := LoadDummyChain()
+	if err != nil {
+		t.Errorf("failed to create test database: %v", err)
+	}
+	definition := `
+function constructor()
+  testall()
+end
+
+function testall()
+  deploytest()
+  sendtest()
+end
+
+function deploytest()
+  src = [[
+  function default()
+    contract.send(system.getSender(), system.getAmount())
+  end
+
+  function getargs(...)
+    tb = {...}
+  end
+
+  abi.payable(default)
+  abi.register(getargs)
+  ]]
+
+  addr = contract.deploy(src)
+  id = 'deploy_src'; system.setItem(id, addr)
+  system.print(id, system.getItem(id))
+
+  korean_char_src = [[
+  function 함수()
+    변수 = 1
+    결과 = 변수 + 3
+    system.print('결과', 결과)
+  end
+
+  abi.register(함수)
+  ]]
+
+  
+  korean_char_src222 = [[
+    function default()
+      contract.send(system.getSender(), system.getAmount())
+    end
+  
+    function getargs(...)
+      tb = {...}
+    end
+  
+    function x()
+    end
+
+    abi.payable(default)
+    abi.register(getargs)
+  ]]
+
+  korean_addr =  contract.deploy(korean_char_src)
+  id = 'korean_char_src'; system.setItem(id, korean_addr)
+  system.print(id, system.getItem(id))
+end
+
+function sendtest()
+  addr = system.getItem("deploy_src")
+  system.print('ADDRESS', addr, system.getAmount())
+  
+  id = 's01'; system.setItem(id,{pcall(function() contract.send(addr, system.getAmount()) end)})
+  system.print(id, system.getItem(id))
+end
+
+function default()
+  -- do nothing
+end
+
+abi.payable(constructor, default)
+abi.register(testall)
+`
+
+	err = bc.ConnectBlock(
+		NewLuaTxAccount("ktlee", 100),
+		NewLuaTxDef("ktlee", "n-deploy", 0, definition),
+	)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 // end of test-cases
