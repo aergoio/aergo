3c1aa7af19202a6aff252f570f398c68701671a1
Hayarobi Park <hayarobipark@gmail.com>
2019-07-25 11:16:54 +0900

    [P2P] Add interface to get network information of server itself.

--- p2p/p2pcommon/others.go
+++ p2p/p2pcommon/others.go
@@ -9,8 +9,12 @@ import (
 
 // PeerAccessor is an interface for a another actor module to get info of peers
 type PeerAccessor interface {
+	// SelfMeta returns peerid, ipaddress and port of server itself
+	SelfMeta() PeerMeta
+
 	GetPeerBlockInfos() []types.PeerBlockInfo
 	GetPeer(ID types.PeerID) (RemotePeer, bool)
+
 }
 
 type SyncManager interface {
