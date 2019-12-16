e5b37966eaa8a0e1400cb088487228de403897ad
Kyungtae Lee <mlogue76@gmail.com>
2019-12-16 17:01:24 +0900

    [debug] Copy service from parent's state

--- contract/vm.c
+++ contract/vm.c
@@ -130,11 +130,8 @@ const char *vm_copy_service(lua_State *L, lua_State *main)
 {
     int err;
     int service;
-
-    err = lua_cpcall(L, cp_getLuaExecContext, &service);
-    if (err != 0) {
-        return lua_tostring(L, -1);
-    }
+    lua_cpcall(L, cp_getLuaExecContext, &service);
+	service = getLuaExecContext(main);
     luaL_set_service(L, service);
     return NULL;
 }
