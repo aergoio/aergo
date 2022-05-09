645faee602510bd82cef21c6fe66456f6ae44636
Tom AI Dev Team <tom.ai.devteam@gmail.com>
2022-05-09 12:06:51 +0000

    fix issue#104

--- cmd/brick/exec/callContract.go
+++ cmd/brick/exec/callContract.go
@@ -28,7 +28,7 @@ func (c *callContract) Syntax() string {
 }
 
 func (c *callContract) Usage() string {
-	return fmt.Sprintf("call <sender_name> <amount> <contract_name> <func_name> `[call_json_str]` `[expected_error_str]`")
+	return fmt.Sprintf("call <sender_name> <amount> <contract_name> <func_name> `[call_json_str]` `[expected_result_str]` `[expected_error_str]`")
 }
 
 func (c *callContract) Describe() string {
@@ -42,20 +42,20 @@ func (c *callContract) Validate(args string) error {
 		return fmt.Errorf("load chain first")
 	}
 
-	_, _, _, _, _, _, err := c.parse(args)
+	_, _, _, _, _, _, _, err := c.parse(args)
 
 	return err
 }
 
-func (c *callContract) parse(args string) (string, *big.Int, string, string, string, string, error) {
+func (c *callContract) parse(args string) (string, *big.Int, string, string, string, string, string, error) {
 	splitArgs := context.SplitSpaceAndAccent(args, false)
 	if len(splitArgs) < 4 {
-		return "", nil, "", "", "", "", fmt.Errorf("need at least 4 arguments. usage: %s", c.Usage())
+		return "", nil, "", "", "", "", "", fmt.Errorf("need at least 4 arguments. usage: %s", c.Usage())
 	}
 
 	amount, success := new(big.Int).SetString(splitArgs[1].Text, 10)
 	if success == false {
-		return "", nil, "", "", "", "", fmt.Errorf("fail to parse number %s", splitArgs[1].Text)
+		return "", nil, "", "", "", "", "", fmt.Errorf("fail to parse number %s", splitArgs[1].Text)
 	}
 
 	callCode := "[]"
@@ -64,10 +64,15 @@ func (c *callContract) parse(args string) (string, *big.Int, string, string, str
 	}
 
 	expectedError := ""
-	if len(splitArgs) == 6 {
-		expectedError = splitArgs[5].Text
-	} else if len(splitArgs) > 6 {
-		return "", nil, "", "", "", "", fmt.Errorf("too many arguments. usage: %s", c.Usage())
+	expectedRes := ""
+
+	if len(splitArgs) >= 6 {
+		expectedRes = splitArgs[5].Text
+	}
+	if len(splitArgs) == 7 {
+		expectedError = splitArgs[6].Text
+	} else if len(splitArgs) > 7 {
+		return "", nil, "", "", "", "", "", fmt.Errorf("too many arguments. usage: %s", c.Usage())
 	}
 
 	return splitArgs[0].Text, //accountName
@@ -75,13 +80,14 @@ func (c *callContract) parse(args string) (string, *big.Int, string, string, str
 		splitArgs[2].Text, //contractName
 		splitArgs[3].Text, //funcName
 		callCode, //callCode
+		expectedRes, //expectedRes
 		expectedError, //expectedError
 		nil
 }
 
 func (c *callContract) Run(args string) (string, uint64, []*types.Event, error) {
 
-	accountName, amount, contractName, funcName, callCode, expectedError, _ := c.parse(args)
+	accountName, amount, contractName, funcName, callCode, expectedRes, expectedError, _ := c.parse(args)
 
 	formattedQuery := fmt.Sprintf("{\"name\":\"%s\",\"args\":%s}", funcName, callCode)
 
@@ -111,6 +117,14 @@ func (c *callContract) Run(args string) (string, uint64, []*types.Event, error) 
 	result := "success"
 	if len(receipt.Ret) > 0 {
 		result += ": " + receipt.Ret
+	}
+	if expectedRes != "" {
+		if expectedRes == receipt.Ret{
+			result = "success: " + "compare successfully, expected result was archived"
+		}else{
+			result = fmt.Sprintf("success: " + "expected: %s, but got: %s", expectedRes, receipt.Ret)
+		}
+
 	}
 	return result, receipt.GasUsed, context.Get().GetEvents(callTx.Hash()), nil
 
