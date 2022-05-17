a6ff552e770ba1cb020ddb6bf018cd7e71933e4e
Sung-Jae Woo <qcollapse53@gmail.com>
2022-05-17 18:01:09 +0900

    [contract] discard events for a failed transaction

--- chain/chainhandle.go
+++ chain/chainhandle.go
@@ -980,6 +980,11 @@ func executeTx(
 	}
 
 	if err != nil {
+		// Reset events on error
+		if contract.HardforkConfig.IsV3Fork(bi.No) {
+			events = nil
+		}
+
 		if !contract.IsRuntimeError(err) {
 			return err
 		}
