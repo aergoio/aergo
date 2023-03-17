08733080c93fef70e0c2b55f4c72f1f3893b66b6
Won Beom Kim <shepelt@gmail.com>
2023-03-17 03:59:36 +0000

    implemented test code for connecting a new block and copying EVM state

--- contract/vm_evm_test.go
+++ contract/vm_evm_test.go
@@ -1,7 +1,10 @@
 package contract
 
 import (
+	"io/ioutil"
 	"math/big"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -12,15 +15,36 @@ import (
 )
 
 func TestEVM(t *testing.T) {
-	// load eth DB and state
-	testState, _ := state.New(common.Hash{}, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
+	// set up levelDB
+	testFile, _ := ioutil.TempFile(os.TempDir(), "temp")
+	testPath, _ := filepath.Abs(filepath.Dir(testFile.Name()))
+	testPath = testPath + "/ethleveldb/test"
+	t.Log("creating temp DB at", testPath)
+	testLevelDB, _ := rawdb.NewLevelDBDatabase(testPath, 128, 1024, "", false)
+	defer testLevelDB.Close()
+
+	// set up state
+	testDB := state.NewDatabase(testLevelDB)
+	stateRoot := common.Hash{}
+	testState, _ := state.New(stateRoot, testDB, nil)
 	if testState == nil {
 		t.Errorf("eth state not created")
 	}
 	t.Log("created eth state")
 
-	// create contract
 	testAddress := common.HexToAddress("0x0a")
+	// create evmCfg
+	evmCfg := vm.Config{
+		Debug: false,
+	}
+
+	// create call cfg
+	testCfg := &runtime.Config{
+		State:     testState,
+		EVMConfig: evmCfg,
+	}
+
+	// configure state #1
 	testState.SetCode(testAddress, []byte{
 		byte(vm.PUSH1), 10,
 		byte(vm.PUSH1), 0,
@@ -33,18 +57,19 @@ func TestEVM(t *testing.T) {
 		byte(vm.RETURN),
 	})
 
-	// execute contract
-	ret, _, err := runtime.Call(testAddress, nil, &runtime.Config{State: testState})
+	// execute contract #1
+	ret, _, err := runtime.Call(testAddress, nil, testCfg)
 	if err != nil {
 		t.Fatal("didn't expect error", err)
 	}
 
-	// test result
+	// test result #1
 	num := new(big.Int).SetBytes(ret)
 	if num.Cmp(big.NewInt(10)) != 0 {
 		t.Error("Expected 10, got", num)
 	}
 
+	// configure state #2
 	testState.SetCode(testAddress, []byte{
 		byte(vm.PUSH1), 10,
 		byte(vm.PUSH1), 0,
@@ -57,8 +82,55 @@ func TestEVM(t *testing.T) {
 		byte(vm.RETURN),
 	})
 
-	// execute contract
+	// execute contract #2
 	ret, _, err = runtime.Call(testAddress, nil, &runtime.Config{State: testState})
+	if err != nil {
+		t.Fatal("didn't expect error", err)
+	}
+
+	// test result #3
+	num = new(big.Int).SetBytes(ret)
+	if num.Cmp(big.NewInt(20)) != 0 {
+		t.Error("Expected 20, got", num)
+	}
+
+	// try creating new state (new block)
+	prevRoot, _ := testState.Commit(true)
+	testState2, _ := state.New(prevRoot, testDB, nil)
+
+	if testState2 == nil {
+		t.Error("Failed to create a new state")
+	}
+
+	// configure new state
+	testState2.SetCode(testAddress, []byte{
+		byte(vm.PUSH1), 31,
+		byte(vm.PUSH1), 0,
+		byte(vm.MSTORE),
+		byte(vm.PUSH1), 32,
+		byte(vm.PUSH1), 0,
+		byte(vm.RETURN),
+	})
+
+	// execute contract again
+	testCfg2 := &runtime.Config{
+		State:     testState2,
+		EVMConfig: evmCfg,
+	}
+
+	ret, _, err = runtime.Call(testAddress, nil, testCfg2)
+	if err != nil {
+		t.Fatal("didn't expect error", err)
+	}
+
+	// test result again
+	num = new(big.Int).SetBytes(ret)
+	if num.Cmp(big.NewInt(31)) != 0 {
+		t.Error("Expected 31, got", num)
+	}
+
+	// test result again prev state
+	ret, _, err = runtime.Call(testAddress, nil, testCfg)
 	if err != nil {
 		t.Fatal("didn't expect error", err)
 	}
@@ -66,6 +138,7 @@ func TestEVM(t *testing.T) {
 	// test result
 	num = new(big.Int).SetBytes(ret)
 	if num.Cmp(big.NewInt(20)) != 0 {
-		t.Error("Expected 20, got", num)
+		t.Error("Expected 10, got", num)
 	}
+
 }
