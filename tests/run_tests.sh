9e557b6aea3fe929eeed998b7cb3d8ecdf22363d
Bernardo Ramos <bernardo@blocko.io>
2023-05-08 07:28:12 +0000

    add run_tests.sh

---
+++ tests/run_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# open the aergo server in testmode to create the config file
+../bin/aergosvr --testmode > logs 2> logs &
+pid=$!
+# wait it create the config file
+sleep 3
+# terminate the server process
+kill $pid
+# enable the block production on the config file
+sed -i 's/^enablebp = false$/enablebp = true/' ~/.aergo/config.toml
+# restart the aergo server in testmode
+../bin/aergosvr --testmode > logs 2> logs &
+pid=$!
+sleep 3
+
+# run the integration tests
+./test-name-service.sh
+
+# terminate the server process
+kill $pid
