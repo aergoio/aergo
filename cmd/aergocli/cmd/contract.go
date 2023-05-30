9740b6ac9ee7e7f2c393eb715f49edb51dd602a2
Bernardo Ramos <bernardo@blocko.io>
2023-05-30 20:35:38 +0000

    [aergocli] check if input is even on isHexString

--- cmd/aergocli/cmd/contract.go
+++ cmd/aergocli/cmd/contract.go
@@ -130,6 +130,11 @@ func init() {
 }
 
 func isHexString(s string) bool {
+  // check is the input has even number of characters
+  if len(s)%2 != 0 {
+    return false
+  }
+  // check if the input contains only hex characters
 	for _, c := range s {
 		if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')) {
 			return false
