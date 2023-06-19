8d86890625d02968fc875967dcb0d10829320a07
Bernardo Ramos <bernardo@blocko.io>
2023-06-19 03:13:19 +0000

    add test-gas-bf

---
+++ tests/test-gas-bf.sh
@@ -0,0 +1,65 @@
+set -e
+
+assert_equals() {
+  local var="$1"
+  local expected="$2"
+
+  if [[ ! "$var" == "$expected" ]]; then
+    echo "Assertion failed: $var != $expected"
+    exit 1
+  fi
+}
+
+assert_contains() {
+  local var="$1"
+  local substring="$2"
+
+  if [[ ! "$var" == *"$substring"* ]]; then
+    echo "Assertion failed: $var does not contain $substring"
+    exit 1
+  fi
+}
+
+
+#../bin/aergocli account import --keystore . --if 47zh1byk8MqWkQo5y8dvbrex99ZMdgZqfydar7w2QQgQqc7YrmFsBuMeF1uHWa5TwA1ZwQ7V6 --password bmttest
+
+
+# deploy
+
+echo "-- deploy --"
+
+../bin/aergoluac --payload ../contract/vm_dummy/test_files/gas_bf.lua > payload.out
+
+txhash=$(../bin/aergocli --keystore . --password bmttest \
+    contract deploy AmPpcKvToDCUkhT1FJjdbNvR4kNDhLFJGHkSqfjWe3QmHm96qv4R \
+    --payload `cat payload.out` | jq .hash | sed 's/"//g')
+
+echo "txhash=$txhash"
+
+sleep 1
+
+../bin/aergocli receipt get $txhash > receipt.json
+
+status=$(cat receipt.json | jq .status | sed 's/"//g')
+address=$(cat receipt.json | jq .contractAddress | sed 's/"//g')
+
+assert_equals "$status" "CREATED"
+
+
+echo "-- call --"
+
+txhash=$(../bin/aergocli --keystore . --password bmttest \
+  contract call AmPpcKvToDCUkhT1FJjdbNvR4kNDhLFJGHkSqfjWe3QmHm96qv4R \
+  $address main "[]" | jq .hash | sed 's/"//g')
+
+sleep 1
+
+../bin/aergocli receipt get $txhash > receipt.json
+
+status=$(cat receipt.json | jq .status | sed 's/"//g')
+ret=$(cat receipt.json | jq .ret | sed 's/"//g')
+gasUsed=$(cat receipt.json | jq .gasUsed | sed 's/"//g')
+
+assert_equals "$status"   "SUCCESS"
+#assert_equals "$ret"      "{}"
+assert_equals "$gasUsed"  "47456460"
