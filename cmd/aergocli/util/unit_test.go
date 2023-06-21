6ad15206dcb009dcd35625b0850cee4eb3c2e9f4
Chang <sjc02183@naver.com>
2023-06-21 08:16:03 +0000

    change aergocli test to use denomination

--- cmd/aergocli/util/unit_test.go
+++ cmd/aergocli/util/unit_test.go
@@ -4,50 +4,61 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/aergoio/aergo/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestPasreUnit(t *testing.T) {
 	amount, err := ParseUnit("1 aergo")
 	assert.NoError(t, err, "parsing aergo")
-	t.Log(amount)
+	assert.Equalf(t, types.NewAergo(1).String(), amount.String(), "parse is failed")
+
 	amount, err = ParseUnit("101 Aergo")
 	assert.NoError(t, err, "parsing Aergo")
-	t.Log(amount)
-	amount, err = ParseUnit("101 AERGO")
-	assert.NoError(t, err, "parsing AERGO")
-	t.Log(amount)
+	assert.Equalf(t, types.NewAergo(101).String(), amount.String(), "parse is failed")
+
 	amount, err = ParseUnit("123 aer")
 	assert.NoError(t, err, "parsing aer")
-	assert.Equal(t, new(big.Int).SetUint64(123), amount, "123 aer")
+	assert.Equalf(t, types.NewAer(123).String(), amount.String(), "parse is failed")
+
+	amount, err = ParseUnit("4567 aer")
+	assert.NoError(t, err, "parsing aer")
+	assert.Equalf(t, types.NewAer(4567).String(), amount.String(), "parse is failed")
+
 	amount, err = ParseUnit("4567 Aer")
-	assert.NoError(t, err, "parsing Aer")
-	assert.Equal(t, new(big.Int).SetUint64(4567), amount, "4567 Aer")
+	assert.NoError(t, err, "parsing aer")
+	assert.Equalf(t, types.NewAer(4567).String(), amount.String(), "parse is failed")
+
 	amount, err = ParseUnit("101 AER")
-	assert.NoError(t, err, "parsing AER")
-	assert.Equal(t, new(big.Int).SetUint64(101), amount, "101 AER")
+	assert.NoError(t, err, "parsing aer")
+	assert.Equalf(t, types.NewAer(101).String(), amount.String(), "parse is failed")
+
 	amount, err = ParseUnit("1 gaer")
 	assert.NoError(t, err, "parsing gaer")
-	assert.Equal(t, new(big.Int).SetUint64(1000000000), amount, "1 gaer")
+	assert.Equalf(t, types.NewGaer(1).String(), amount.String(), "parse is failed")
+
 	amount, err = ParseUnit("1010")
-	assert.Equal(t, new(big.Int).SetUint64(1010), amount, "1010")
 	assert.NoError(t, err, "parsing implicit unit")
-	t.Log(amount)
+	assert.Equalf(t, types.NewAer(1010).String(), amount.String(), "parse is failed")
 }
 
 func TestPasreDecimalUnit(t *testing.T) {
 	amount, err := ParseUnit("1.01 aergo")
 	assert.NoError(t, err, "parsing point aergo")
-	assert.Equal(t, new(big.Int).SetUint64(1010000000000000000), amount, "converting result")
+	assert.Equal(t, types.NewAer(1010000000000000000), amount, "converting result")
+
 	amount, err = ParseUnit("1.01 gaer")
 	assert.NoError(t, err, "parsing point gaer")
-	assert.Equal(t, new(big.Int).SetUint64(1010000000), amount, "converting result")
+	assert.Equalf(t, types.NewAer(1010000000), amount, "converting result")
+
 	amount, err = ParseUnit("0.123456789012345678 aergo")
 	assert.NoError(t, err, "parsing point")
-	t.Log(amount)
+	assert.Equalf(t, types.NewAer(123456789012345678), amount, "converting result")
+
 	amount, err = ParseUnit("0.100000000000000001 aergo")
 	assert.NoError(t, err, "parsing point max length of decimal")
-	t.Log(amount)
+	assert.Equalf(t, types.NewAer(100000000000000001), amount, "converting result")
+
 	amount, err = ParseUnit("499999999.100000000000000001 aergo")
 	assert.NoError(t, err, "parsing point max length of decimal")
 	t.Log(amount)
@@ -108,10 +119,10 @@ func TestConvertUnit(t *testing.T) {
 }
 
 func TestParseUnit(t *testing.T) {
-	n100 := big.NewInt(100)
-	n1000 := big.NewInt(1000)
-	OneGaer := big.NewInt(1000000000)
-	OneAergo := big.NewInt(1000000000).Mul(OneGaer, OneGaer)
+	n100 := types.NewAer(100)
+	n1000 := types.NewAer(1000)
+	OneGaer := types.NewGaer(1)
+	OneAergo := types.NewAergo(1)
 
 	tests := []struct {
 		name    string
@@ -120,22 +131,22 @@ func TestParseUnit(t *testing.T) {
 		wantErr bool
 	}{
 		{"TNum", "10000", big.NewInt(10000), false},
-		{"TNumDot", "1000.5", big.NewInt(0), true},
-		{"TNum20pow", "100000000000000000000", big.NewInt(1).Mul(OneAergo, n100), false},
+		{"TNumDot", "1000.5", new(big.Int), true},
+		{"TNum20pow", "100000000000000000000", new(big.Int).Mul(OneAergo, n100), false},
 		{"TAer", "10000aer", big.NewInt(10000), false},
-		{"TAerDot", "1000.5aer", big.NewInt(0), true},
-		{"TAer20pow", "100000000000000000000", big.NewInt(1).Mul(OneAergo, n100), false},
-		{"TGaer", "1000gaer", big.NewInt(1).Mul(OneGaer, n1000), false},
-		{"TGaerDot", "1000.21245gaer", big.NewInt(1).Add(big.NewInt(0).Mul(OneGaer, n1000), big.NewInt(212450000)), false},
+		{"TAerDot", "1000.5aer", new(big.Int), true},
+		{"TAer20pow", "100000000000000000000", new(big.Int).Mul(OneAergo, n100), false},
+		{"TGaer", "1000gaer", new(big.Int).Mul(OneGaer, n1000), false},
+		{"TGaerDot", "1000.21245gaer", new(big.Int).Add(new(big.Int).Mul(OneGaer, n1000), big.NewInt(212450000)), false},
 		{"TGaerDot2", "0.21245gaer", big.NewInt(212450000), false},
 		{"TGaerDot3", ".21245gaer", big.NewInt(212450000), false},
-		{"TGaer11pow", "100000000000gaer", big.NewInt(1).Mul(OneAergo, n100), false},
-		{"TAergo", "100aergo", big.NewInt(1).Mul(OneAergo, n100), false},
-		{"TAergoDot", "1000.00000000021245aergo", big.NewInt(1).Add(big.NewInt(0).Mul(OneAergo, n1000), big.NewInt(212450000)), false},
-		{"TWrongNum", "100d0.321245", big.NewInt(1), true},
-		{"TWrongNum2", "100d0.321245aergo", big.NewInt(1), true},
-		{"TWrongUnit", "100d0.321245argo", big.NewInt(1), true},
-		{"TWrongUnit", "100d0.321245ear", big.NewInt(1), true},
+		{"TGaer11pow", "100000000000gaer", new(big.Int).Mul(OneAergo, n100), false},
+		{"TAergo", "100aergo", new(big.Int).Mul(OneAergo, n100), false},
+		{"TAergoDot", "1000.00000000021245aergo", new(big.Int).Add(new(big.Int).Mul(OneAergo, n1000), big.NewInt(212450000)), false},
+		{"TWrongNum", "100d0.321245", new(big.Int), true},
+		{"TWrongNum2", "100d0.321245aergo", new(big.Int), true},
+		{"TWrongUnit", "100d0.321245argo", new(big.Int), true},
+		{"TWrongUnit", "100d0.321245ear", new(big.Int), true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
