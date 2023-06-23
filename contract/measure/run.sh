7523abc206945ae8dcbe4a9c1598e514b42511bc
Bernardo Ramos <bernardo@blocko.io>
2023-06-23 03:39:46 +0000

    fix build of contract/measure tool

--- contract/measure/run.sh
+++ contract/measure/run.sh
@@ -4,8 +4,14 @@ LUAJIT=../../libtool/bin/luajit-2.1.0-beta3
 
 make -C../.. libluajit-clean
 make -C../.. DASM_XFLAGS='-D MEASURE' libluajit
+echo ""
+echo "---------------------------------------------"
+echo "Measure execution of op.lua"
 $LUAJIT -joff op.lua
+echo "---------------------------------------------"
+echo ""
 
 make -C../.. libluajit-clean
 make -C../.. libluajit
+echo "---------------------------------------------"
 go run -tags measure main.go | jq -r .message | tr -d \"
