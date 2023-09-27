1364d154b291d77dd24b232e4d7c24e63c10be28
Bernardo Ramos <bernardo@blocko.io>
2023-09-27 03:01:37 +0000

    check deployed contracts

--- contract/vm_dummy/test_files/token-deployer.lua
+++ contract/vm_dummy/test_files/token-deployer.lua
@@ -11,11 +11,13 @@ end
 function deploy_tokens(n1, n2)
 
   for n = 1,n1 do
-    contract.call(arc1_factory:get(), "new_token", "Test", "TST", 18, 1000, {burnable=true, mintable=true, pausable=true, blacklist=true, all_approval=true, limited_approval=true})
+    address = contract.call(arc1_factory:get(), "new_token", "Test", "TST", 18, 1000, {burnable=true, mintable=true, pausable=true, blacklist=true, all_approval=true, limited_approval=true})
+    assert(contract.call(address, "symbol") == "TST", "deployed contract is not working")
   end
 
   for n = 1,n2 do
-    contract.call(arc2_factory:get(), "new_arc2_nft", "Test NFT 1", "NFT1", null, {burnable=true, mintable=true, metadata=true, pausable=true, blacklist=true, approval=true, searchable=true, non_transferable=true, recallable=true})
+    address = contract.call(arc2_factory:get(), "new_arc2_nft", "Test NFT 1", "NFT1", null, {burnable=true, mintable=true, metadata=true, pausable=true, blacklist=true, approval=true, searchable=true, non_transferable=true, recallable=true})
+    assert(contract.call(address, "symbol") == "NFT1", "deployed contract is not working")
   end
 
 end
