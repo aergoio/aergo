b556341b335ff26d4eead3f289ecdc0114ef8065
Bernardo Ramos <bernardo@blocko.io>
2023-10-30 20:39:39 +0000

    tests: fix spaces [skip ci]

--- contract/vm_dummy/test_files/pcall-events-0.lua
+++ contract/vm_dummy/test_files/pcall-events-0.lua
@@ -12,27 +12,27 @@ end
 
 function test_pcall()
   local s, r = pcall(do_work, parent:get(), "pcall")
-	assert(s == false, "call not failed")
-	return r
+  assert(s == false, "call not failed")
+  return r
 end
 
 function test_xpcall()
   local s, r = xpcall(do_work, error_handler, parent:get(), "xpcall")
-	assert(s == false, "call not failed")
-	return r
+  assert(s == false, "call not failed")
+  return r
 end
 
 function test_contract_pcall()
   local s, r = contract.pcall(do_work, parent:get(), "contract.pcall")
-	assert(s == false, "call not failed")
-	return r
+  assert(s == false, "call not failed")
+  return r
 end
 
 function do_work(contract_address, caller)
   contract.event("inside " .. caller .. " before")
   local r = contract.call(contract_address, "call_me")
   contract.event("inside " .. caller .. " after")
-	return r
+  return r
 end
 
 abi.register(test_pcall, test_xpcall, test_contract_pcall)
--- contract/vm_dummy/test_files/pcall-events-1.lua
+++ contract/vm_dummy/test_files/pcall-events-1.lua
@@ -14,31 +14,31 @@ function test_pcall()
   contract.event("before pcall")
   local s, r = pcall(do_work, parent:get(), "pcall")
   contract.event("after pcall")
-	assert(s == false, "call not failed")
-	return r
+  assert(s == false, "call not failed")
+  return r
 end
 
 function test_xpcall()
   contract.event("before xpcall")
   local s, r = xpcall(do_work, error_handler, parent:get(), "xpcall")
   contract.event("after xpcall")
-	assert(s == false, "call not failed")
-	return r
+  assert(s == false, "call not failed")
+  return r
 end
 
 function test_contract_pcall()
   contract.event("before contract.pcall")
   local s, r = contract.pcall(do_work, parent:get(), "contract.pcall")
   contract.event("after contract.pcall")
-	assert(s == false, "call not failed")
-	return r
+  assert(s == false, "call not failed")
+  return r
 end
 
 function do_work(contract_address, caller)
   contract.event("inside " .. caller .. " before")
   local r = contract.call(contract_address, "call_me")
   contract.event("inside " .. caller .. " after")
-	return r
+  return r
 end
 
 abi.register(test_pcall, test_xpcall, test_contract_pcall)
