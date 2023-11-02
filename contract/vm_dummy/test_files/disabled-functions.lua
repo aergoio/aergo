9cee33b1bdd40183f5da93eb358e2008b1d80b01
Bernardo Ramos <bernardo@blocko.io>
2023-11-02 16:58:11 +0000

    add tests for disabled Lua functions

---
+++ contract/vm_dummy/test_files/disabled-functions.lua
@@ -0,0 +1,40 @@
+
+function check_disabled_functions()
+
+  -- check the disabled modules
+  assert(os == nil, "os is available")
+  assert(io == nil, "io is available")
+  assert(debug == nil, "debug is available")
+  assert(jit == nil, "jit is available")
+  assert(ffi == nil, "ffi is available")
+  assert(coroutine == nil, "coroutine is available")
+  assert(package == nil, "package is available")
+
+  -- check the disabled functions
+  assert(collectgarbage == nil, "collectgarbage is available")
+  assert(gcinfo == nil, "gcinfo is available")
+  assert(module == nil, "module is available")
+  assert(require == nil, "require is available")
+  assert(dofile == nil, "dofile is available")
+  assert(load == nil, "load is available")
+  assert(loadlib == nil, "loadlib is available")
+  assert(loadfile == nil, "loadfile is available")
+  assert(loadstring == nil, "loadstring is available")
+  assert(print == nil, "print is available")
+
+  local success, result = pcall(function() newproxy() end)
+  assert(success == false and result:match(".* 'newproxy' not supported"), "newproxy is available")
+  local success, result = pcall(function() setfenv() end)
+  assert(success == false and result:match(".* 'setfenv' not supported"), "setfenv is available")
+  local success, result = pcall(function() getfenv() end)
+  assert(success == false and result:match(".* 'getfenv' not supported"), "getfenv is available")
+
+  -- make sure the tostring does not return a pointer
+  local tab = {}
+  assert(not tostring(type):match("0x[%x]+"), "tostring returns a pointer for function")
+  assert(not tostring(system):match("0x[%x]+"), "tostring returns a pointer for internal table")
+  assert(not tostring(tab):match("0x[%x]+"), "tostring returns a pointer for table")
+
+end
+
+abi.register(check_disabled_functions)
