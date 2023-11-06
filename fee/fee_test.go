c005c1453523df4df2723225a1f352d11cdcfc8b
kch <sjc02183@naver.com>
2023-11-06 06:15:37 +0000

    add fee tests

--- fee/fee_test.go
+++ fee/fee_test.go
@@ -1,10 +1,12 @@
 package fee
 
 import (
+	"math"
 	"math/big"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTxBaseFee(t *testing.T) {
@@ -56,24 +58,66 @@ func TestTxExecuteFee(t *testing.T) {
 	for _, test := range []struct {
 		forkVersion       int32
 		isQuery           bool
+		dbUpdateTotalSize int64
 		gasPrice          *big.Int
 		usedGas           uint64
-		dbUpdateTotalSize int64
 		expectFee         *big.Int
 	}{
-		// TODO
-	} {
+		// v1 or isQuery - fee by dbUpdateTotalSize
+		{1, false, 0, nil, 0, Gaer(0)},
+		{1, false, 200, nil, 0, Gaer(0)},
+		{1, false, 201, nil, 0, Gaer(5000)},
+		{1, false, 300, nil, 0, Gaer(500000)},
+		{2, true, 1200, nil, 0, Gaer(5000000)},
+		{3, true, 10200, nil, 0, Gaer(50000000)},
 
+		// after v2 - fee by gas * gasPrice
+		{2, false, 0, Gaer(1), 0, Gaer(0)},
+		{2, false, 0, Gaer(1), 200, Gaer(200)},
+		{3, false, 0, Gaer(5), 200, Gaer(1000)},
+		{3, false, 0, Gaer(5), 100000, Gaer(500000)},
+	} {
 		resultFee := TxExecuteFee(test.forkVersion, test.isQuery, test.gasPrice, test.usedGas, test.dbUpdateTotalSize)
 		assert.EqualValues(t, test.expectFee, resultFee, "TxExecuteFee(forkVersion:%d, isQuery:%t, gasPrice:%s, usedGas:%d, dbUpdateTotalSize:%d)", test.forkVersion, test.isQuery, test.gasPrice, test.usedGas, test.dbUpdateTotalSize)
 	}
 }
 
 func TestTxMaxFee(t *testing.T) {
+	DisableZeroFee()
+	defer EnableZeroFee()
 
+	for _, test := range []struct {
+		forkVersion int32
+		lenPayload  int
+		gasLimit    uint64
+		gasPrice    *big.Int
+		balance     *big.Int
+		expectErr   bool
+		expectFee   *big.Int
+	}{
+		// v1 - max fee by payload
+		{1, 0, 0, nil, nil, false, Gaer(2000000)}, // base 0.002 AERGO
+		{1, 1, 0, nil, nil, false, Gaer(1025000000)},
+		{1, 200, 0, nil, nil, false, Gaer(1025000000)},
+		{1, 201, 0, nil, nil, false, Gaer(1025005000)},
+		{1, 300, 0, nil, nil, false, Gaer(1025500000)},
+		{1, 1200, 0, nil, nil, false, Gaer(1030000000)},
+
+		// after v2 - max fee by gas limit
+		{2, 0, 1000, Gaer(5), nil, true, nil}, // smaller than base fee
+		{2, 0, 100000, Gaer(1), nil, false, Gaer(100000)},
+		{2, 0, 100000, Gaer(5), nil, false, Gaer(500000)},
+
+		{2, 0, 0, Gaer(5), Gaer(500000), false, Gaer(500000)}, // no gas limit - get from balance
+		{2, 0, 0, Gaer(5), Gaer(1000000), false, Gaer(1000000)},
+		{2, 0, 0, Gaer(5), Gaer(-1), false, new(big.Int).Mul(new(big.Int).SetUint64(math.MaxUint64), Gaer(5))}, // no gas limit and no balance = max gas
+	} {
+		resultFee, err := TxMaxFee(test.forkVersion, test.lenPayload, test.gasLimit, test.balance, test.gasPrice)
+		require.Equal(t, test.expectErr, err != nil, "TxMaxFee(forkVersion:%d, lenPayload:%d, gasLimit:%d, balance:%s, gasPrice:%s)", test.forkVersion, test.lenPayload, test.gasLimit, test.balance, test.gasPrice)
+		require.EqualValues(t, test.expectFee, resultFee, "TxMaxFee(forkVersion:%d, lenPayload:%d, gasLimit:%d, balance:%s, gasPrice:%s)", test.forkVersion, test.lenPayload, test.gasLimit, test.balance, test.gasPrice)
+	}
 }
 
-// TODO : replace to types.NewAmount after resolve cycling import
 func Gaer(n int) *big.Int {
 	return big.NewInt(0).Mul(big.NewInt(int64(n)), big.NewInt(int64(1e9)))
 }
