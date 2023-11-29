2c3a1dd6a4314823d21dd474fe8e601515c104e1
Hayarobi Park <hayarobipark@gmail.com>
2023-11-29 10:54:25 +0900

    remove preload

--- chain/chainhandle.go
+++ chain/chainhandle.go
@@ -653,22 +653,14 @@ func (e *blockExecutor) execute() error {
 	// Receipt must be committed unconditionally.
 	if !e.commitOnly {
 		defer contract.CloseDatabase()
-		var preloadTx *types.Tx
-		numTxs := len(e.txs)
-		for i, tx := range e.txs {
-			// if tx is not the last one, preload the next tx
-			if i != numTxs-1 {
-				preloadTx = e.txs[i+1]
-				contract.RequestPreload(e.BlockState, e.bi, preloadTx, tx, contract.ChainService)
-			}
+		logger.Trace().Int("txCount", len(e.txs)).Msg("executing txs (maybe for connecting block)")
+		for _, tx := range e.txs {
 			// execute the transaction
 			if err := e.execTx(e.BlockState, types.NewTransaction(tx)); err != nil {
 				//FIXME maybe system error. restart or panic
 				// all txs have executed successfully in BP node
 				return err
 			}
-			// mark the next preload tx to be executed
-			contract.SetPreloadTx(preloadTx, contract.ChainService)
 		}
 
 		if e.validateSignWait != nil {
