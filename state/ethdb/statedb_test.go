649c6955f618c9200043b05795eab90da63b84dc
kch <sjc02183@naver.com>
2023-11-29 08:31:38 +0000

    update go version

--- state/ethdb/statedb_test.go
+++ state/ethdb/statedb_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,8 +17,7 @@ func TestState(t *testing.T) {
 	sdbOld, err := NewStateDB(0, nil, db)
 	require.NoError(t, err)
 
-	expectRoot := []byte{86, 232, 31, 23, 27, 204, 85, 166, 255, 131, 69, 230, 146, 192, 248, 110, 91, 72, 224, 27, 153, 108, 173, 192, 1, 98, 47, 181, 227, 99, 180, 33}
-	require.Equal(t, sdbOld.Root(), expectRoot, "root mismatch with expect")
+	require.Equal(t, sdbOld.Root(), types.EmptyRootHash.Bytes(), "root mismatch with expect")
 	require.Equal(t, sdbOld.Root(), sdbOld.evmStateDB.IntermediateRoot(false).Bytes(), "root mismatch with IntermediateRoot(false)")
 	require.Equal(t, sdbOld.Root(), sdbOld.evmStateDB.IntermediateRoot(true).Bytes(), "root mismatch with IntermediateRoot(true)")
 
@@ -29,7 +29,6 @@ func TestState(t *testing.T) {
 
 	newRoot, err := sdbOld.Commit()
 	require.NoError(t, err)
-	fmt.Println("oldRoot", expectRoot)
 	fmt.Println("newRoot", newRoot)
 	sdbOld.Close()
 
@@ -38,10 +37,6 @@ func TestState(t *testing.T) {
 
 	fmt.Println(sdbNew.evmStateDB.GetBalance(addr).String())
 
-	dump := sdbOld.evmStateDB.RawDump(nil)
-	for k, v := range dump.Accounts {
-		fmt.Println("addr :", k, "balance :", v.Balance)
-	}
-	// iter := memoryDB.NewIterator(nil, nil)
-	// fmt.Println("key", iter.Key(), "value", iter.Value())
+	iter := db.Store.NewIterator(nil, nil)
+	fmt.Println("key", iter.Key(), "value", iter.Value())
 }
