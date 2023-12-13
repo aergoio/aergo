171edd456e9c181ddeb69e7341b2a02742eeaf47
gokch <sjc02183@naver.com>
2023-12-13 07:47:26 +0000

    fix test

--- state/account_test.go
+++ state/account_test.go
@@ -17,10 +17,9 @@ func TestEthAccount(t *testing.T) {
 	}{
 		{[]byte{3, 112, 238, 158, 33, 17, 101, 89, 68, 95, 155, 198, 187, 127, 233, 38, 180, 46, 175, 134, 228, 158, 146, 64, 79, 134, 218, 92, 224, 53, 224, 191, 124},
 			"AmPJht1vphFthrzB5TTHBRsqJ3yezmPgb9P8V8v4aCbCLpEg2WYC", // base58check encoded from compressed pubkey ( 33 length )
-			"0x17cC364f7b86772b7bbd40e23e0217eDC7edbCCF"},          // hex encoded from uncompressed pubkey ( 65 length )
+			"0xDe00eFa73EF966c5a28DbD6E2C9A2830a3453207"},          // hex encoded from uncompressed pubkey ( 65 length )
 	} {
 		acc := key.NewAddressEth(test.account)
-
 		require.Equal(t, test.evmAccount, acc.String())
 	}
 }
