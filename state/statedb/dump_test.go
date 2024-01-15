42960a7d9ac1888cdf19e6d23243d8df77f2fd86
gokch <sjc02183@naver.com>
2024-01-15 08:24:23 +0000

    add dump test

--- state/statedb/dump_test.go
+++ state/statedb/dump_test.go
@@ -92,3 +92,62 @@ func TestDumpContract(t *testing.T) {
 	"root": "9NfkNYKP6KKZbQ2S3CDkeWqWMfxv2zfNgETCPoqWkDmP"
 }`)
 }
+
+func TestDumpStorage(t *testing.T) {
+	initTest(t)
+	defer deinitTest()
+
+	code := "testcode"
+	codeHash := common.Hasher([]byte(code))
+	// set code
+	err := saveData(store, codeHash, []byte(code))
+	require.NoError(t, err, "failed to save code")
+
+	// set contract state
+	err = stateDB.PutState(testAccount, &types.State{
+		Balance:  types.NewAmount(1, types.Aergo).Bytes(),
+		Nonce:    1,
+		CodeHash: common.Hasher([]byte(code)),
+	})
+	require.NoError(t, err, "failed to put state")
+
+	// set contract storage
+	scs, err := OpenContractStateAccount([]byte("test_address"), stateDB)
+	require.NoError(t, err, "failed to open contract state account")
+	scs.SetData([]byte("test_storage1"), []byte("test_value1"))
+	scs.SetData([]byte("test_storage2"), []byte("test_value2"))
+	scs.SetData([]byte("test_storage3"), []byte("test_value3"))
+	StageContractState(scs, stateDB)
+
+	err = stateDB.Update()
+	require.NoError(t, err, "failed to update")
+	err = stateDB.Commit()
+	require.NoError(t, err, "failed to commit")
+
+	dump, err := stateDB.RawDump()
+	require.NoError(t, err)
+
+	jsondata, err := json.MarshalIndent(dump, "", "\t")
+	require.NoError(t, err)
+
+	require.Equal(t, string(jsondata), `{
+	"accounts": {
+		"9RhQjznbYXqMQG1GmuYSsvoCYe5bnCPZCTnT6ZvohkxN": {
+			"code": "testcode",
+			"state": {
+				"balance": "1000000000000000000",
+				"codeHash": "6GBoUd26XJnkj6wGs1L6fLg8jhuVXSTVUNWzoXsjeHoh",
+				"nonce": 1,
+				"sqlRecoveryPoint": 0,
+				"storageRoot": "9SVveGGrFXJtoVFFiGpWZ1TmHmKLPnqoYmS7AGxzxgdL"
+			},
+			"storage": {
+				"BVsyGJb6L5qLr8EPcM78Wd5NgZz3cjC1jM2FxpmZFrBm": "Vs5LyU62cV3qLve",
+				"ByeMAs5g3t233iEGkEzMYoN4UrnaJPJ6TGNdY2vs9MBg": "Vs5LyU62cV3qLvd",
+				"q6MAgzMsY2iJcukzs5x7M9WFmN4bT9AiqzviD1DcSZX": "Vs5LyU62cV3qLvc"
+			}
+		}
+	},
+	"root": "9bQx52KdKfMkVdakKEWQLBscgkMiFwd2Zx2hr7u1GYam"
+}`)
+}
