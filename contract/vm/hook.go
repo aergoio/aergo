37bbea867351c548812782c59e0906e0b2c3cbe6
Bernardo Ramos <bernardo@blocko.io>
2025-02-18 15:59:16 +0000

    fix setCountHook

--- contract/vm/hook.go
+++ contract/vm/hook.go
@@ -9,13 +9,16 @@ package main
 import "C"
 
 func (ce *executor) setCountHook(limit C.int) {
-	if ce == nil || ce.L == nil || ce.err != nil || IsGasSystem() {
-		C.vm_set_timeout_hook(ce.L)
+	if ce == nil || ce.L == nil || ce.err != nil {
 		return
 	}
-	if hardforkVersion >= 2 {
-		C.vm_set_timeout_count_hook(ce.L, limit)
+	if IsGasSystem() {
+		C.vm_set_timeout_hook(ce.L)
 	} else {
-		C.vm_set_count_hook(ce.L, limit)
+		if hardforkVersion >= 2 {
+			C.vm_set_timeout_count_hook(ce.L, limit)
+		} else {
+			C.vm_set_count_hook(ce.L, limit)
+		}
 	}
 }
