68ed5e72f645701591f1f04f895f65f4d67d5e6d
Bernardo Ramos <bernardo@blocko.io>
2025-03-28 13:39:22 -0300

    test import of Lua package

--- tests/test-import-2.lua
+++ tests/test-import-2.lua
@@ -1,4 +1,5 @@
 import "test-name-service.lua"
+import "https://raw.githubusercontent.com/aergoio/aergo-contract-ex/refs/heads/master/contracts/typecheck/typecheck.lua"
 
 function test2()
   return "test2"
