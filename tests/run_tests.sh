6c37e8aa33a07a6f0e626f95071cce13ecbd7d09
Bernardo Ramos <bernardo@blocko.io>
2025-08-21 06:46:51 +0000

    add support for HashTableDB

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -100,8 +100,8 @@ function set_version() {
   echo "now test hardfork version $version"
   # get the current block number / height
   block_no=$(../bin/aergocli blockchain | jq .height | sed 's/"//g')
-  # increment 3 numbers
-  block_no=$((block_no+3))
+  # increment 5 numbers
+  block_no=$((block_no+5))
   # terminate the server process(es)
   stop_nodes
   # save the hardfork config on the config file(s)
