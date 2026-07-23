a555c025f5fcc7c2b306562176f569ffece45509
Bernardo Ramos <bernardo@blocko.io>
2026-07-23 19:01:51 +0000

    notify syncer on busy paths and verify sync target hash

--- consensus/chain/block.go
+++ consensus/chain/block.go
@@ -1,6 +1,7 @@
 package chain
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"time"
@@ -209,7 +210,24 @@ func SyncChain(hs *component.ComponentHub, targetHash []byte, targetNo types.Blo
 	logger.Info().Str("peer", p2putil.ShortForm(peerID)).Uint64("no", targetNo).
 		Str("hash", enc.ToString(targetHash)).Msg("request to sync for consensus")
 
-	notiC := make(chan error)
+	verifyTarget := func() error {
+		result, err := hs.RequestFuture(message.ChainSvc, &message.GetBlockByNo{BlockNo: targetNo},
+			time.Second, "consensus/chain.SyncChain.verifyTarget").Result()
+		block, err := message.GetHelper().ExtractBlockFromResponseAndError(result, err)
+		if err != nil {
+			return err
+		}
+		if block == nil || !bytes.Equal(block.BlockHash(), targetHash) {
+			return fmt.Errorf("%w: block %d hash does not match snapshot", ErrSyncChain, targetNo)
+		}
+		return nil
+	}
+
+	if best := GetBestBlock(hs); best != nil && best.BlockNo() >= targetNo {
+		return verifyTarget()
+	}
+
+	notiC := make(chan error, 1)
 	hs.Tell(message.SyncerSvc, &message.SyncStart{PeerID: peerID, TargetNo: targetNo, NotifyC: notiC})
 
 	// wait end of sync every 1sec
@@ -224,7 +242,13 @@ func SyncChain(hs *component.ComponentHub, targetHash []byte, targetNo types.Blo
 		}
 	}
 
+	if err := verifyTarget(); err != nil {
+		logger.Error().Err(err).Uint64("no", targetNo).
+			Str("hash", enc.ToString(targetHash)).
+			Msg("sync completed at a different block")
+		return err
+	}
+
 	logger.Info().Str("peer", p2putil.ShortForm(peerID)).Msg("succeeded to sync for consensus")
-	// TODO check best block is equal to target Hash/no
 	return nil
 }
--- syncer/syncerservice.go
+++ syncer/syncerservice.go
@@ -83,6 +83,7 @@ var (
 var (
 	ErrFinderInternal = errors.New("error finder internal")
 	ErrSyncerPanic    = errors.New("syncer panic")
+	ErrSyncerBusy     = errors.New("syncer is already running")
 )
 
 type ErrSyncMsg struct {
@@ -318,17 +319,30 @@ func (syncer *Syncer) handleSyncStart(msg *message.SyncStart) error {
 	var err error
 	var bestBlock *types.Block
 
+	notify := func(err error) {
+		if msg.NotifyC == nil {
+			return
+		}
+		select {
+		case msg.NotifyC <- err:
+		default:
+			logger.Debug().Msg("failed to notify rejected sync request")
+		}
+	}
+
 	logger.Debug().Uint64("targetNo", msg.TargetNo).Str("peer", p2putil.ShortForm(msg.PeerID)).Msg("syncer requested")
 
 	if syncer.isRunning {
 		logger.Debug().Uint64("targetNo", msg.TargetNo).Msg("skipped syncer is running")
-		return nil
+		notify(ErrSyncerBusy)
+		return ErrSyncerBusy
 	}
 
 	//TODO skip sync in reorgnizing
-	bestBlock, _ = syncer.chain.GetBestBlock()
+	bestBlock, err = syncer.chain.GetBestBlock()
 	if err != nil {
 		logger.Error().Err(err).Msg("error getting block in syncer")
+		notify(err)
 		return err
 	}
 
@@ -337,6 +351,7 @@ func (syncer *Syncer) handleSyncStart(msg *message.SyncStart) error {
 	if msg.TargetNo <= bestBlockNo {
 		logger.Debug().Uint64("targetNo", msg.TargetNo).Uint64("bestNo", bestBlockNo).
 			Msg("skipped syncer. requested no is too low")
+		notify(nil)
 		return nil
 	}
 
